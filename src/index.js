dd55389330af784f6238e3eb10ddd2a557b8d0d2
Wilfried Canirinka <canirinkawilfried@gmail.com>
2020-03-25 12:34:01 +0200

    indes.js file updated

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 import dom from './dom';
 
+const todoList = [
+  {
+    name: 'Build A House',
+    todos: [
+      {
+        title: 'Consult Architect',
+        description: 'Have a face meeting with the architect',
+        dueDate: '2020-03-25',
+        priority: 'High',
+        completed: false,
+      },
+      {
+        title: 'Meet with Project Engineer',
+        description: 'Meet with the Project Engineer to discuss project scope',
+        dueDate: '2020-03-27',
+        priority: 'High',
+        completed: false,
+      },
+    ],
+  },
+];
+const storedTodo = JSON.parse(localStorage.getItem('todolist'));
+if (!storedTodo) {
+  localStorage.setItem('todolist', JSON.stringify(todoList));
+}
+
 dom.renderProjects();
